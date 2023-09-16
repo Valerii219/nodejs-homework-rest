62137bfb29e5b1359902ecf749528733e496caa0
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-09-16 13:43:01 +0200

    save changes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const app = require("./app");
 
 const mongoose = require('mongoose');
-const DB_HOST = 'mongodb+srv://Valerii21:Valerii21.@cluster0.ojkzz5m.mongodb.net/?retryWrites=true&w=majority'
+const DB_HOST = 'mongodb+srv://Valerii21:Valerii21.@cluster0.ojkzz5m.mongodb.net/db-contacts?retryWrites=true&w=majority'
 
-mongoose.connect()
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set('strictQuery', true)
+mongoose.connect(DB_HOST)
+.then(()=>{
+  app.listen(3000)
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+})
