1415f46b3366aef0843fc97b16911c7437b7270d
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-09-16 18:56:43 +0200

    + moongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require('mongoose')
+
+const DB_HOST = 'mongodb+srv://Valerii21:Valerii21.@cluster0.ojkzz5m.mongodb.net/?retryWrites=true&w=majority'
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+.then(()=>{
+  app.listen(3000)
+  console.log("Database connection successful");
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+})
+
