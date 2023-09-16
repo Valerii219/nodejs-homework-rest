710e73acfc1651e1f67279486713909f6d30820d
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-09-16 13:34:51 +0200

    a

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
+const mongoose = require('mongoose');
+const DB_HOST = 'mongodb+srv://Valerii21:Valerii21.@cluster0.ojkzz5m.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect()
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
