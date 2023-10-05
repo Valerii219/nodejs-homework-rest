f933e840284832c683b235b080a4bb774963fa72
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-10-05 19:58:14 +0200

    fix errors

--- middlewares/contactOwner.js
+++ middlewares/contactOwner.js
@@ -1,24 +1,101 @@
 const { Contact } = require("../models/contact");
-const HttpError = require("../helpers/HttpError")
-const checkContactOwner = async (req, res, next) => {
-  const { user } = req; // Поточний користувач (залогінений)
+const HttpError = require("../helpers/HttpError");
 
-  const { id } = req.params;
-
-  if (id) {
-    try {
-      const contact = await Contact.findOne({ _id: id, owner: user._id });
-
-      // Перевірити, чи контакт існує та чи є поточний користувач його власником
-      if (!contact) {
-        return res.status(403).json({ message: "Access denied" });
-      }
-    } catch {
-      next(HttpError(500, "Server error"));
-    }
+const getOwner = async (req, res, next) => {
+  try {
+    // Знайдемо всі контакти, які належать поточному користувачу
+    const contacts = await Contact.find({ owner: req.user._id });
+    res.json(contacts);
+  } catch (error) {
+    next(error);
   }
-
-  next(); // Перейти до наступного middleware або обробки запиту
 };
 
-module.exports = { checkContactOwner };
+const getIdOwner = async (req, res, next) => {
+  const contactId = req.params.id;
+
+  try {
+    // Знайдемо контакт за ідентифікатором, який належить поточному користувачу
+    const contact = await Contact.findOne({ _id: contactId, owner: req.user._id });
+
+    if (!contact) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteIdOwner = async (req, res, next) => {
+  const contactId = req.params.id;
+
+  try {
+    // Знайдемо та видалимо контакт за ідентифікатором, який належить поточному користувачу
+    const result = await Contact.deleteOne({ _id: contactId, owner: req.user._id });
+
+    if (result.deletedCount === 0) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    res.json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postOwner = async (req, res, next) => {
+  try {
+    const { _id: owner } = req.user;
+    const result = await Contact.create({ ...req.body, owner });
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const putIdOwner = async (req, res, next) => {
+  const contactId = req.params.id;
+  const updatedContactData = req.body;
+
+  try {
+    // Оновимо контакт за ідентифікатором, який належить поточному користувачу
+    const updatedContact = await Contact.findOneAndUpdate(
+      { _id: contactId, owner: req.user._id },
+      updatedContactData,
+      { new: true }
+    );
+
+    if (!updatedContact) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const patchFavoriteOwner = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { error } = updateFavoriteSchema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getOwner, getIdOwner, deleteIdOwner, postOwner, putIdOwner, patchFavoriteOwner  };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,20 +5,20 @@ const ctrl = require("../../controllers/contacts.js");
 const { addSchema } = require("../../models/contact"); // Правильний імпорт
 const { isValidId } = require("../../middlewares/isValidId");
 const authenticate = require("../../middlewares/authenticate");
-const  {checkContactOwner}  = require("../../middlewares/contactOwner");
+const  {getOwner, getIdOwner, deleteIdOwner, postOwner, putIdOwner, patchFavoriteOwner }  = require("../../middlewares/contactOwner");
 
 
+router.get("/", authenticate, getOwner,  ctrl.getAll );
 
-router.get("/", authenticate, checkContactOwner, ctrl.getAll);
+  router.get("/:id", authenticate, getIdOwner,  validateBody(addSchema), isValidId, ctrl.getByContactId )
 
-router.get("/:id", authenticate, checkContactOwner, validateBody(addSchema), isValidId, ctrl.getByContactId);
+  router.delete("/:id", authenticate, deleteIdOwner, isValidId,  ctrl.deleteByContactId )
 
-router.post("/", authenticate,  checkContactOwner, ctrl.add);
+  router.post("/", authenticate, postOwner,  ctrl.add)
 
-router.delete("/:id", authenticate, isValidId, checkContactOwner, ctrl.deleteByContactId);
+router.put("/:id", authenticate, putIdOwner, isValidId,  validateBody(addSchema), ctrl.updateByContactId )
 
-router.put("/:id", authenticate, isValidId, checkContactOwner, validateBody(addSchema), ctrl.updateByContactId);
 
-router.patch("/:id/favorite", authenticate, checkContactOwner, isValidId,  ctrl.updateStatusContact);
+  router.patch("/:id/favorite", patchFavoriteOwner, authenticate)
 
 module.exports = router;
