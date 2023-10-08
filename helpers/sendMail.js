0b4a589838adf9ef2cc67bee8e21d79b1a5871c6
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-10-08 17:34:08 +0200

    save

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,29 +1,29 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
-const {UKR_PASSWORD} = process.env;
+const { UKR_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host:"smtp.ukr.net",
-    port:"465",
-    secure:true,
-    auth:{
-        user:"valera_arl@ukr.net",
-        pass:UKR_PASSWORD
-    }
-} 
+  host: "smtp.ukr.net",
+  port: "465",
+  secure: true,
+  auth: {
+    user: "valera_arl@ukr.net",
+    pass: UKR_PASSWORD,
+  },
+};
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-    try {
-        const email = { ...data, from: "valera_arl@ukr.net" };
-        await transport.sendMail(email);
-        console.log("Email sent successfully");
-        return true;
-    } catch (error) {
-        console.error("Email sending error:", error);
-        return false;
-    }
-}
+  try {
+    const email = { ...data, from: "valera_arl@ukr.net" };
+    await transport.sendMail(email);
+    console.log("Email sent successfully");
+    return true;
+  } catch (error) {
+    console.error("Email sending error:", error);
+    return false;
+  }
+};
 
 module.exports = sendEmail;
